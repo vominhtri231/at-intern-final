0bd42d663510a45be62bae5b4e5d9c0d3c61d7e4
Tai Nguyen D <tai.nguyen@asiantech.vn>
2018-08-29 08:58:24 +0700

    init project

---
+++ app/src/androidTest/java/internship/asiantech/a2018summerfinal/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package internship.asiantech.a2018summerfinal
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("internship.asiantech.a2018summerfinal", appContext.packageName)
+    }
+}
