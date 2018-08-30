6cec1345d6f57759d1c3bfb634e685c4c43520c5
at-lambui <lam.bui@asiantech.vn>
2018-08-30 14:52:13 +0700

    complete ui remoteview

--- app/src/main/java/internship/asiantech/a2018summerfinal/MainActivity.kt
+++ app/src/main/java/internship/asiantech/a2018summerfinal/MainActivity.kt
@@ -7,6 +7,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.remoteview_playingmusic)
+        setContentView(R.layout.activity_main)
     }
 }
